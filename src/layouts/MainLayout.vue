cbd155d3fd1e31b40249891e2a31059cb0954fde
umutaltin <umut.altin@live.com>
2021-07-15 22:40:52 +0300

    Umut

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header class="bg-white text-grey-8 shadow-2">
+      <q-toolbar>
+        <q-toolbar-title class="row justify-between items-center">
+          <q-btn flat icon="keyboard_backspace" />
+          Title
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar.png" />
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container style="background: #eff3f4">
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-white shadow-3 text-grey-8">
+      <q-toolbar>
+        <q-toolbar-title class="row justify-between q-py-md items-center">
+          <q-btn round flat icon="home" color="grey-8"  />
+          <q-btn round flat icon="people" color="grey-8"  />
+          <q-btn round icon="add" text-color="white" color="purple"  />
+           <q-btn round flat icon="insights" color="grey-8"  />
+          <q-btn round flat icon="notifications" color="grey-8"  />
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-page class="text-grey-8 q-pa-md">
+    <!-- User Name -->
+    <section>
+      <p class="q-ma-none" style="font-size: 16px">Hi Yavuz.</p>
+      <p class="text-bold text-grey-10" style="font-size: 18px">Get In Shape</p>
+    </section>
+    <!-- Notifi Card -->
+    <section>
+      <q-card class="orange-card text-white">
+        <q-card-section>
+          <q-btn dense unelevated rounded style="background : #ffffff30" text-color="white" class="text-capitalize q-px-md" label="Entry Level" />
+          <p class="q-mt-sm q-mb-none" style="font-size: 28px">Sholder Press</p>
+          <p style="font-size: 16px; color : #ffffff90">
+            16 sholder workout <br>
+            vidios for you
+          </p>
+        </q-card-section>
+        <q-card-section class="q-pt-none">
+         <div class="round q-pa-xs inline-block" style="background : #ffffff30; border-radius : 60px">
+            <q-icon name="play_circle_filled" color="white" size="xl" />
+         </div>
+         <span class="q-pa-sm" style="font-size : 16px">34 Minutes</span>
+        </q-card-section>
+        <div class="img" />
+      </q-card>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
+
+<style lang="scss">
+
+.orange-card{
+  background-color: $card-orange;
+}
+
+.img{
+  position: absolute;
+  background-image: url("../assets/travel.svg");
+  background-size: 350px;
+  background-repeat: no-repeat;
+  bottom: -55px;
+  right: -110px;
+  width: 350px;
+  height: 350px;
+}
+
+</style>
+
+
+
